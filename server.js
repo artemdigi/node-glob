297acf89c6db7f80f2b7cf785b2b98686754e611
artemdigi <artem.digi@gmail.com>
2022-06-07 02:41:54 +0700

    init

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require('express')
+var glob = require('glob')
+
+const app = express();
+const port = process.env.NODE_PORT || 3004;
+const {TwingEnvironment, TwingLoaderFilesystem} = require('twing');
+
+app.use(express.static('public_html'));
+
+let loader = new TwingLoaderFilesystem('./templates');
+let twing = new TwingEnvironment(loader, {
+    auto_reload: true,
+    cache: false,
+    debug: true
+});
+
+app.get('/', function (req, res) {
+    let options = {
+        'cwd': './templates'
+    }
+
+    var html = '<html><body><h1>Шаблоны:</h1>'
+
+    glob('*.html.twig', options, function (er, files) {
+        console.log(files)
+        console.log(html)
+
+        html += '<ul>'
+        for (const file of files) {
+            let li = '<li>' + file + '</li>';
+            html += li
+        }
+        html += '</ul>'
+    })
+
+    html += '</body></html>'
+
+    console.log(html)
+
+    res.send(html)
+});
+
+app.get('/:name', function (req, res) {
+    let now = new Date()
+
+    console.log(now + ' -> ' + req.params.name)
+    twing.render(req.params.name + '.html.twig', req.params).then((output) => {
+        res.end(output);
+    });
+});
+
+app.listen(port, () => {
+    console.log('Node.js Express server listening on port http://localhost:' + port);
+});
